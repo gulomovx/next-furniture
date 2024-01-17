51207374ecd535155acf480d0b0d58bd47ae7abf
gulomovx <xgulomov447@gmail.com>
2024-01-17 16:57:01 +0500

    updated

--- src/components/header/HeaderInfo.jsx
+++ src/components/header/HeaderInfo.jsx
@@ -17,22 +17,22 @@ const HeaderInfo = () => {
   return (
     <div className="w-full lg:my-24 my-12">
       <div className="container bg-blue-900 p-16 rounded-[30px] shadow-xl  flex lg:justify-between flex-col lg:flex-row text-2xl items-center gap-16 ">
-        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue">
+        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue lg:text-[20px] text-[18px]  ">
           <FaTruckFast size={30} className='font-extrabold  text-center' />
           <h1 className="">1500+</h1>
           <p className="">Product delivery</p>
         </div>
-        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue">
+        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue lg:text-[20px] text-[18px]">
           <SiCoinmarketcap size={30} className='font-extrabold text-white text-center' />
           <h1 className="">2000+</h1>
           <p className="">Sale per month</p>
         </div>
-        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue">
+        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue lg:text-[20px] text-[18px]">
           <GiProgression size={30} className='font-extrabold text-white  text-center' />
           <h1 className="">25%</h1>
           <p className="">Growth per year</p>
         </div>
-        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue">
+        <div className="flex flex-col  justify-center text-white items-center gap-2 text-blue lg:text-[20px] text-[18px]">
           <LiaSitemapSolid size={30} className='font-extrabold text-white  text-center' />
           <h1 className="">100+</h1>
           <p className="">Branches </p>
